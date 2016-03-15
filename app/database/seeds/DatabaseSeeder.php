d89b8ee4dac4a114735228741d3bb4270975bb95
Kirill Savenkov <leoxpw@gmail.com>
2016-03-16 00:44:53 +0200

    categorySeeder and successfull seeding

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -24,6 +24,22 @@ class CategoriesTableSeeder extends Seeder {
 		foreach($jsonCat as $categoriesParent){
 			$cat = new Category();
 			$cat->title = $categoriesParent->title;
+			$cat->slug = last(explode('/', $categoriesParent->slug));
+			$cat->save();
+			foreach($categoriesParent->children  as $catChild1){
+				$cat1 = new Category();
+				$cat1->title = $catChild1->title;
+				$cat1->slug = last(explode('/', $catChild1->slug));
+				$cat1->parent_id = $cat->id;
+				$cat1->save();
+				foreach($catChild1->children as $catChild2){
+					$cat2 = new Category();
+					$cat2->title = $catChild2->title;
+					$cat2->slug = last(explode('/', $catChild2->slug));
+					$cat2->parent_id = $cat1->id;
+					$cat2->save();
+				}
+			}
 		}
 	}
 }
